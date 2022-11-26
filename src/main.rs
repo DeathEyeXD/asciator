b1d35adc08c622d4e6a248b5cc52d4740444d6a2
Karol Zwolak <karolzwolak7@gmail.com>
2022-11-26 12:01:44 +0100

    Improve ascii grayscale

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use colored::*;
+// use colored::*;
 use image::GenericImageView;
 
 fn main() {
@@ -31,25 +31,31 @@ fn run(path: &str, invert: bool) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-const CHAR_MATRIX: &str = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+// const CHAR_MATRIX: &str = " `.,^\":;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+const GRAYSCALE_CHARS: &str = " .,;!vlLFE$";
+// const CHAR_MATRIX: &str = r#"@MBHENR#KWXDFPQASUZbdehx*8Gm&04LOVYkpq5Tagns69owz$CIu23Jcfry%1v7l+it[] {}?j|()=~!-/<>\"^_';,:`. "#;
 
 fn convert_pixel(brightness: u8) -> char {
-    let len = CHAR_MATRIX.len();
+    let len = GRAYSCALE_CHARS.len();
 
-    let index = (brightness as f32 / 255.0 * (len - 1) as f32) as usize;
-    return CHAR_MATRIX.chars().nth(index).unwrap();
+    let index = (brightness as f64 / 255.0 * (len - 1) as f64).round() as usize;
+    return GRAYSCALE_CHARS.chars().nth(index).unwrap();
 }
 
 fn scale_down(img: &mut image::DynamicImage) {
     let (width, height) = img.dimensions();
     let scale = 100.0 / width as f64;
-    let new_width = (width as f64 * scale) as u32;
-    let new_height = (height as f64 * scale) as u32;
+    let new_width = (width as f64 * scale).round();
+    let new_height = (height as f64 * scale).round();
 
-    *img = img.resize(new_width, new_height, image::imageops::FilterType::Nearest)
+    *img = img.resize(
+        new_width as u32,
+        new_height as u32,
+        image::imageops::FilterType::Nearest,
+    )
 }
 
-fn map_image(mut img: image::DynamicImage) -> Vec<String> {
+fn map_image(img: image::DynamicImage) -> Vec<String> {
     let mut rows = vec!["".to_string(); img.height() as usize];
     for (_, y, brightness) in img.into_luma8().enumerate_pixels() {
         rows[y as usize].push(convert_pixel(brightness[0]));
